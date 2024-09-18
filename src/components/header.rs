10175a639556e3b4d5326d464528ac5f5837a714
lemorage <one.lemorage@gmail.com>
2024-09-18 09:54:52 +0800

    refactor: tweak the header component and styles

--- src/components/header.rs
+++ src/components/header.rs
@@ -5,17 +5,12 @@ pub fn header(current_page: &str) -> Markup {
         @if current_page != "home" {
             header {
                 nav {
+                    a href="/" { "Home" }
                     @if current_page != "about" {
                         a href="/about" { "About Me" }
                     }
-                    @if current_page != "about" {
-                        span.nav-separator { "|" }
-                    }
                     @if current_page != "portfolio" {
                         a href="/portfolio" { "Portfolio" }
-                    }
-                    @if current_page != "portfolio" {
-                        span.nav-separator { "|" }
                     }
                     @if current_page != "blog" {
                         a href="/blog" { "Blog" }
