ac317e159d7b19cd0a2f59e39028db7056bedb2c
lemorage <one.lemorage@gmail.com>
2024-11-09 14:48:21 +0800

    feat: update footer to display the current year dynamically

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -1,9 +1,20 @@
 use maud::{html, Markup};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn current_year() -> i32 {
+    let secs_since_epoch = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs();
+
+    1970 + (secs_since_epoch / 31_557_600) as i32
+}
 
 pub fn footer() -> Markup {
+    let year = current_year();
     html! {
         div class="footer-main" {
-            "Glenn Miao " (maud::PreEscaped("&copy;")) " 2024. All rights reserved."
+            "Glenn Miao " (maud::PreEscaped("&copy;")) " " (year) ". All rights reserved."
         }
     }
 }
