2d60a88b48a7472d2f5c346448542edb58772b9a
lemorage <one.lemorage@gmail.com>
2024-12-02 13:49:09 +0800

    chore: remove obsolete proj and redirect to new site

--- assets/index.html.a453a256.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-534676ce","path":"/tags/algorithms/","title":"Algorithms | Tags","lang":"en-US","frontmatter":{"title":"Algorithms | Tags","layout":"Tags","blog":{"type":"category","name":"Algorithms","key":"tag"}},"excerpt":"","headers":[],"git":{},"filePathRelative":null}');export{t as data};
